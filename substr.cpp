03a589b61b8e7af344c9d44c1fd3d93a85458b8b
Priyank <priyankt68@gmail.com>
2014-04-29 09:06:07 +0530

    string rotation problem

---
+++ substr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	
+	std::string str = "priyank";   
+	str.append(str);
+
+
+	cout << str << endl;
+
+
+	return 0;
+}
