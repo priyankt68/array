a52168b680e0fe3bbd84eb3a5f83e1bb7e08101e
Priyank Trivedi <priyankt68@gmail.com>
2014-07-20 13:02:40 +0530

    Create tutorial_intro.cpp in arrays and sorting

---
+++ hacker_rank/arrays_and_sorting/tutorial_intro.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int v,n;
+    cin >> v;
+    cin >> n;
+    
+    int arr[n];
+    
+    for(int i=0;i<n;i++)
+    {
+            cin >> arr[i];
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]== v)
+        cout << i;
+    }
+    
+    
+    return 0;
+    
+}
